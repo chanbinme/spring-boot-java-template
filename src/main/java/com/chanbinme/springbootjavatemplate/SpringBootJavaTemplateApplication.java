79c05ed129777dad12da0d125cee71070a350d7e
chanbinme <gksmfcksqls@gmail.com>
2025-08-24 17:43:43 +0900

    git init

---
+++ src/main/java/com/chanbinme/springbootjavatemplate/SpringBootJavaTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.chanbinme.springbootjavatemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJavaTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chanbinme/springbootjavatemplate/SpringBootJavaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanbinme.springbootjavatemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavaTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
